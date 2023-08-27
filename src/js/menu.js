9df2edf280ceaedacf4770d8cb5aeba5eeeb68c5
Alexander <finods@gmail.com>
2023-08-27 16:05:14 +0300

    update js + added id to classes

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,28 +1,29 @@
 (() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const mobileMenu = document.querySelector('.menu-container');
+  const openMenuBtn = document.querySelector('.open-menu');
+  const closeMenuBtn = document.querySelector('.close-menu');
+  const links = document.querySelectorAll('.mob-menu-item');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    const isMenuOpen = mobileMenu.classList.contains('is-open');
+    mobileMenu.classList.toggle('is-open', !isMenuOpen);
+    document.body.style.overflow = isMenuOpen ? 'auto' : 'hidden';
   };
+
+  links.forEach(link => {
+    link.addEventListener('click', toggleMenu);
+  });
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
+  const mediaQuery = window.matchMedia('(min-width: 768px)');
+  const handleMediaChange = e => {
+    if (e.matches) {
+      mobileMenu.classList.remove('is-open');
+      document.body.style.overflow = 'auto';
+    }
+  };
+
+  mediaQuery.addListener(handleMediaChange);
 })();
