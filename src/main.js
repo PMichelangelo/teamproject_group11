66dee339b8033df1d8854326d3d2feec3fe42514
Victor-Lars-Petrenko <bistrayastrela123@gmail.com>
2023-08-27 18:46:07 +0300

    Виконав додаткове завдання на секції Price
    Виконав додаткове завдання на секції Price. JS згенеровано за допомогою ChatGPT. Поки не можу виконати плавне зникнення псевдоелемента, як на відео

--- src/main.js
+++ src/main.js
@@ -13,3 +13,29 @@
     document.body.classList.toggle('no-scroll');
   }
 })();
+
+const priceItems = document.querySelectorAll('.price-item-name');
+
+function hidePseudoElement() {
+  priceItems.forEach(item => {
+    item.classList.remove('active');
+  });
+}
+
+function togglePseudoElement(item) {
+  priceItems.forEach(otherItem => {
+    if (otherItem !== item) {
+      otherItem.classList.remove('active');
+    }
+  });
+  item.classList.toggle('active');
+}
+
+priceItems.forEach(item => {
+  item.addEventListener('click', event => {
+    event.stopPropagation();
+    togglePseudoElement(item);
+  });
+});
+
+document.addEventListener('click', hidePseudoElement);
